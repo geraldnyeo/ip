855d954c4cbedf972464e7156c0f4093f24bc8be
geraldnyeo <geraldnyeo@gmail.com>
2026-08-20 10:38:55 +0800

    Greet user and exit

--- src/main/java/Cassava.java
+++ src/main/java/Cassava.java
@@ -1,4 +1,6 @@
 public class Cassava {
+    private static String name = "Cassava";
+
     public static void main(String[] args) {
         String banner = "  ____                               \n" +
                 " / ___|__ _ ___ ___  __ ___   ____ _\n" +
@@ -6,5 +8,12 @@ public class Cassava {
                 "| |__| (_| \\__ \\__ \\ (_| |\\ V / (_| |\n" +
                 " \\____\\__,_|___/___/\\__,_| \\_/ \\__,_|\n";
         System.out.println(banner);
+
+        System.out.println("__________________________________________________");
+        System.out.println("Hi! I'm " + name + ".");
+        System.out.println("How can I help you today?");
+
+        System.out.println("__________________________________________________");
+        System.out.println("Bye! See you again soon.");
     }
 }
