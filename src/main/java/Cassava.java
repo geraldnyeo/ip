6622f25463aa975924b45ac4c20144e6f8d12411
geraldnyeo <geraldnyeo@gmail.com>
2026-08-20 11:05:36 +0800

    Feature: echo commands and bye command
    Chatbot can only greet the user and immediately exit.
    
    Two new features have been added.
    * Users can now type commands the chatbot. Cassava will echo commands it
      does not recognise.
    * Users can now type the 'bye' command to exit the chatbot.

--- src/main/java/Cassava.java
+++ src/main/java/Cassava.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
+
 public class Cassava {
     private static String name = "Cassava";
 
-    public static void main(String[] args) {
+    public static void printIntro() {
         String banner = "  ____                               \n" +
                 " / ___|__ _ ___ ___  __ ___   ____ _\n" +
                 "| |   / _` / __/ __|/ _` \\ \\ / / _` |\n" +
@@ -12,8 +14,39 @@ public class Cassava {
         System.out.println("__________________________________________________");
         System.out.println("Hi! I'm " + name + ".");
         System.out.println("How can I help you today?");
+    }
+
+    public static String scanUserInput() {
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("__________________________________________________");
+        String command = scanner.nextLine();
+        return command;
+    }
+
+    public static boolean handleCommand(String command) {
+        if (command.equals("bye")) {
+            return handleExit();
+        } else {
+            System.out.println("__________________________________________________");
+            System.out.println(command);
+            return false;
+        }
+    }
+
+    public static boolean handleExit() {
+        System.out.println("__________________________________________________");
         System.out.println("Bye! See you again soon.");
+        return true;
+    }
+
+    public static void main(String[] args) {
+        printIntro();
+
+        boolean exit = false;
+        while (!exit) {
+            String command = scanUserInput();
+            exit = handleCommand(command);
+        }
     }
 }
