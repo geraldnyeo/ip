bb2121f77554d324f902b7f36bcb6b27d93866a4
geraldnyeo <geraldnyeo@gmail.com>
2026-08-20 10:27:00 +0800

    Rename Duke to Cassava

---
+++ src/main/java/Cassava.java
@@ -0,0 +1,10 @@
+public class Cassava {
+    public static void main(String[] args) {
+        String banner = "  ____                               \n" +
+                " / ___|__ _ ___ ___  __ ___   ____ _\n" +
+                "| |   / _` / __/ __|/ _` \\ \\ / / _` |\n" +
+                "| |__| (_| \\__ \\__ \\ (_| |\\ V / (_| |\n" +
+                " \\____\\__,_|___/___/\\__,_| \\_/ \\__,_|\n";
+        System.out.println(banner);
+    }
+}
